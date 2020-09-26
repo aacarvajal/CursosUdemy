151ce156f3e5d671959ca78c4ac671c45f7b8cef
Adrian <adrycarpe94@gmail.com>
2020-09-26 14:32:09 +0200

    curso de cero a avanzado
    primer commit del curso (metodos)

---
+++ Curso para Aprender C#  de Cero a Experto - 2020/MetodosDemo/MetodosDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MetodosDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine("Hello World!");
+
+            Console.SetWindowSize(20, 20);
+
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.Beep();
+
+            Console.ReadLine();*/
+
+            /*Console.WriteLine("Hello World!");
+            
+            methodExample();
+
+            Console.WriteLine(2+5);
+            methodExample();*/
+
+            string campo = Console.ReadLine();
+
+            Saludar(campo);
+
+            Console.ReadLine();
+
+        }
+
+        static void Saludar(string campo)
+        {
+
+            Console.WriteLine("Hello World! " + campo);
+
+        }
+
+        public static void methodExample()
+        {
+
+            Console.ReadLine();
+            Console.Clear();
+            Console.Beep();
+
+        }
+
+    }
+}
