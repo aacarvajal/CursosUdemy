5fa56113ef5f6b952378103efed846116daa2eee
Adrian <adrycarpe94@gmail.com>
2020-09-29 21:01:25 +0200

    Excepciones

---
+++ Curso para Aprender C#  de Cero a Experto - 2020/Excepciones1Demos/Excepciones1Demos/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones1Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int valor = 10;
+            //Console.WriteLine(valor / 0);
+
+            //string num = "a10";
+            //int valor = int.Parse(num);
+
+            //string num = "9999999999";
+            //int valor = int.Parse(num);
+
+            //Console.WriteLine(valor);
+
+            //try
+            //{
+            //      operaciones que hay que comprobar
+            //}
+            //catch (tipo de excepcion ex)
+            //{
+            //    operacion a ejecutar en caso de excepcion
+            //}
+
+            try
+            {
+
+                int valor = 10;
+                Console.WriteLine(valor / 0);
+
+            }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+
+            try
+            {
+
+                string num = "a10";
+                int valor1 = int.Parse(num);
+
+            }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+            catch (FormatException e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+
+            try
+            {
+
+                string num = "9999999999";
+                int valor = int.Parse(num);
+
+            }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+            catch (FormatException e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+            catch (OverflowException e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+            catch(Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Curso para Aprender C#  de Cero a Experto - 2020/Excepciones2Demos/Excepciones2Demos/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones2Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int entero = int.MaxValue;
+
+            //checked
+            //{
+
+            //    entero++;//comprueba la posible excepcion
+
+            //}
+
+            //entero = unchecked(entero++);
+
+            //Console.WriteLine(entero);
+
+            int mes = int.Parse(Console.ReadLine());
+
+            try
+            {
+
+                Console.WriteLine(Zodiaco(mes));
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+
+                Console.WriteLine("Relleno");
+
+            }
+            
+
+            Console.ReadLine();
+
+        }
+
+        static string Zodiaco(int num)
+        {
+
+            string res = string.Empty;
+
+            switch (num)
+            {
+
+                case 1:
+                    res = "Aries";
+                    break;
+                case 2:
+                    res = "Tauro";
+                    break;
+                case 3:
+                    res = "Geminis";
+                    break;
+                case 4:
+                    res = "Cancer";
+                    break;
+                case 5:
+                    res = "Leo";
+                    break;
+                case 6:
+                    res = "Virgo";
+                    break;
+                case 7:
+                    res = "Libra";
+                    break;
+                case 8:
+                    res = "Escorpio";
+                    break;
+                case 9:
+                    res = "Sagitario";
+                    break;
+                case 10:
+                    res = "Capricornio";
+                    break;
+                case 11:
+                    res = "Acuario";
+                    break;
+                case 12:
+                    res = "Piscis";
+                    break;
+                default:
+                    throw new InvalidOperationException("MAL");
+                    break;
+            }
+
+            return res;
+
+        }
+    }
+}
