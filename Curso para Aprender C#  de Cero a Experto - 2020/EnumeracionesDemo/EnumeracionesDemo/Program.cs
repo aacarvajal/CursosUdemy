a05c85510919a4062b1313bfdeedb8c1a53ea7e9
Adrian <adrycarpe94@gmail.com>
2020-10-03 18:25:03 +0200

    Seccion 9 - Creando tipos valor a traves de enumeraciones

---
+++ Curso para Aprender C#  de Cero a Experto - 2020/EnumeracionesDemo/EnumeracionesDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeracionesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Administrador - 1
+            //Desarrollador - 2
+            //Usuario final - 3
+
+            Rol tipoUsuario = Rol.Administrador;
+
+            Console.WriteLine((int)tipoUsuario);
+
+            if (tipoUsuario == Rol.Administrador)
+            {
+
+                tipoUsuario++;
+
+            }
+
+            Rol? tu2 = null;
+
+            string temporal = tipoUsuario.ToString();
+
+            switch (tipoUsuario)
+            {
+                case Rol.Administrador:
+                    Console.WriteLine("Admin");
+                    break;
+                case Rol.Desarrollador:
+                    Console.WriteLine("Develop");
+                    break;
+                case Rol.UsuarioFinal:
+                    Console.WriteLine("Final User");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public static void Validar(Rol r)
+        {
+
+
+
+        }
+
+    }
+}
---
+++ Curso para Aprender C#  de Cero a Experto - 2020/EnumeracionesDemo/EnumeracionesDemo/Rol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeracionesDemo
+{
+    enum Rol : short
+    {
+
+        Administrador = 5, //los siguientes toman el siguiente valor a partir del valor que este obtiene
+        Desarrollador = 10,
+        UsuarioFinal = 10
+
+    }
+}
