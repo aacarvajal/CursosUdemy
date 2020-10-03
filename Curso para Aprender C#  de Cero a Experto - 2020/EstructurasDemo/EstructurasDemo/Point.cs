baba3d5ab44a26b4ef4cab03fe99a7d955eb25bb
Adrian <adrycarpe94@gmail.com>
2020-10-03 18:43:55 +0200

    Seccion 10 - Creando tipos de valor a traves de estructuras

---
+++ Curso para Aprender C#  de Cero a Experto - 2020/EstructurasDemo/EstructurasDemo/Point.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasDemo
+{
+    struct StructPoint
+    {
+
+        //una estructura es un tipo por valor y se copia en el stack (pila)
+        //una clase solo copia la referencia y ambas referencias apuntando al mismo objeto
+
+        public int x;
+        public int y;
+
+        //no se puede declarar un constructor por defecto (vacio)porque el compilador siempre genera un parametro
+        //al contrario que en una clase, que lo genera si no has creado tu mismo uno
+        //siempre especifica 3 posibles valores para la estructura (0,false y null)
+        //si no se inicializan todas las variables, marcara un fallo en la variable faltante
+
+        //en una clase se puede declarar una variable desde su definicion, mientras que en una estructura no
+        //public Point(int x, int y)
+        //{
+
+        //    this.x = x;
+        //    this.y = y;
+
+        //}
+
+        //public int Calcular()
+        //{
+
+        //    return x * y;
+
+        //}
+
+    }
+
+    class ClassPoint
+    {
+
+        public int x;
+        public int y;
+
+    }
+}
---
+++ Curso para Aprender C#  de Cero a Experto - 2020/EstructurasDemo/EstructurasDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //si no se inicializa dara fallo en el metodo print
+            //Point? p = new Point();
+
+            //Print(p);
+
+            //Console.WriteLine();
+
+            //StructPoint sp1 = new StructPoint();
+            //StructPoint sp2 = sp1;
+
+            //Console.WriteLine(sp1.x);
+            //Console.WriteLine(sp2.x);
+
+            //sp1.x++;
+
+            //Console.WriteLine(sp1.x);
+            //Console.WriteLine(sp2.x);
+
+            ClassPoint cp1 = new ClassPoint();
+            ClassPoint cp2 = cp1;
+
+            Console.WriteLine(cp1.x);
+            Console.WriteLine(cp2.x);
+
+            cp1.x++;
+
+            Console.WriteLine(cp1.x);
+            Console.WriteLine(cp2.x);
+
+            Console.ReadLine();
+
+        }
+
+        //static void Print(Point? p)
+        //{
+
+        //    Console.WriteLine(p.Value.x);
+
+        //}
+
+    }
+}
