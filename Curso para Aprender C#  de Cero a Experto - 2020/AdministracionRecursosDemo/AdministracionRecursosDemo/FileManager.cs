3aa251e11bc6ee4c88797d428acdb843d874cafa
Adrian <adrycarpe94@gmail.com>
2020-10-26 20:16:57 +0100

    Seccion 15 - Recoleccion de basura y administracion de recursos

---
+++ Curso para Aprender C#  de Cero a Experto - 2020/AdministracionRecursosDemo/AdministracionRecursosDemo/FileManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionRecursosDemo
+{
+    class FileManager : IDisposable
+    {
+
+        FileStream reader = null;
+
+        public FileManager(string filePath)
+        {
+            //using (FileStream reader = File.Open(filePath, FileMode.Open))
+            //{
+
+            //    byte[] b = new byte[1024];
+
+            //    UTF8Encoding temp = new UTF8Encoding();
+
+            //    while (reader.Read(b, 0, b.Length) > 0)
+            //    {
+
+            //        Console.WriteLine(temp.GetString(b));
+
+            //    }
+
+            //}             
+
+            //try
+            //{
+
+                reader = File.Open(filePath, FileMode.Open);
+
+                byte[] b = new byte[1024];
+
+                UTF8Encoding temp = new UTF8Encoding();
+
+                while (reader.Read(b, 0, b.Length) > 0)
+                {
+
+                    Console.WriteLine(temp.GetString(b));
+
+                }
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+
+            //    reader.Close();
+
+            //}
+
+        }
+
+        ~FileManager()
+        {
+
+            System.Diagnostics.Trace.WriteLine("Archivo cerrado");
+
+        }
+
+        void IDisposable.Dispose()
+        {
+            reader?.Dispose();//? pregunta si es nula o no, si no lo es, se ejecuta el dispose()
+        }
+
+        //void IDisposable.Dispose()
+        //{
+        //    ((IDisposable)reader).Dispose();
+        //}
+    }
+}
---
+++ Curso para Aprender C#  de Cero a Experto - 2020/AdministracionRecursosDemo/AdministracionRecursosDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionRecursosDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using(FileManager fm = new FileManager(@"C:\Users\Adrian\Documents\prueba.txt"))
+            {
+            }
+
+            using (FileManager fm2 = new FileManager(@"C:\Users\Adrian\Documents\prueba.txt"))
+            {
+            }
+
+            //FileManager fm = new FileManager(@"C:\Users\Adrian\Documents\prueba.txt");
+            //FileManager fm2 = new FileManager(@"C:\Users\Adrian\Documents\prueba.txt");
+
+            Console.ReadLine();
+
+        }
+    }
+}
