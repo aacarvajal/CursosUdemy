64352a40902a1b6818614360151223be4aef3510
Adrian <adrycarpe94@gmail.com>
2020-11-01 20:35:10 +0100

    Seccion 16 - Implementando propiedades para el acceso a campos

---
+++ Curso para Aprender C#  de Cero a Experto - 2020/PropiedadesDemo/PropiedadesDemo/Printer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropiedadesDemo
+{
+    class Printer
+    {
+
+        int x;
+        int y;
+
+        public Printer(int x, int y)
+        {
+
+            this.x = VerificarX(x);
+            this.y = VerificarY(y);
+
+        }
+
+        public void Print()
+        {
+
+            Console.SetCursorPosition(x, y);
+
+            Console.WriteLine("X");
+
+        }
+
+        private int VerificarX(int x)
+        {
+
+            if (x < 0 || x > 50)
+            {
+                throw new ArgumentOutOfRangeException("error en X");
+            }
+
+            return x;
+
+        }
+
+        private int VerificarY(int x)
+        {
+
+            if (y < 0 || y > 60)
+            {
+                throw new ArgumentOutOfRangeException("error en Y");
+            }
+
+            return y;
+
+        }
+
+        public int ObtenerX()
+        {
+
+            return x;
+
+        }
+        public int ObtenerY()
+        {
+
+            return y;
+
+        }
+
+        public void AsignarX(int x)
+        {
+
+            this.x = VerificarX(x);
+
+        }
+
+        public void AsignarY(int y)
+        {
+
+            this.y = VerificarX(y);
+
+        }
+
+    }
+}
---
+++ Curso para Aprender C#  de Cero a Experto - 2020/PropiedadesDemo/PropiedadesDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropiedadesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Printer p = new Printer(10, 15);
+
+            p.Print();
+
+            int x = p.ObtenerX();
+            p.AsignarX(x + 10);
+
+            p.Print();
+
+            Console.ReadLine();
+
+        }
+    }
+}
